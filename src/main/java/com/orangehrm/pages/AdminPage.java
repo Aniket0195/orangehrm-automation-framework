18ccfa5180d8225d51bce360bfc14272008112c3
Aniket0195 <aniketgaikwad0195@gmail.com>
2026-06-13 23:29:57 +0530

    added adminpage

---
+++ src/main/java/com/orangehrm/pages/AdminPage.java
@@ -0,0 +1,15 @@
+package com.orangehrm.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+public class AdminPage {
+	
+	public AdminPage(WebDriver driver)
+	{
+		super(driver);
+	}
+	
+	
+
+}
---
+++ src/test/java/com/orangehrm/tests/AdminTest.java
@@ -0,0 +1,31 @@
+package com.orangehrm.tests;
+
+import org.testng.annotations.Test;
+
+import com.orangehrm.pages.WebElement;
+
+public class AdminTest extends LoginTest{
+  @Test
+  public void f() {
+  }
+  
+  @Test
+	public void clickAdminPage() {
+		//WebElement admindashboard = driver.find
+		
+	}
+	
+	
+	@Test
+	public void enterUsername()
+	{
+		
+	}
+	
+	@Test
+	public void selectRole()
+	{
+		
+	}
+  
+}
