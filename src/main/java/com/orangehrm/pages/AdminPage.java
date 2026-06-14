5792c5c8fcc6966f76112afe243891d7382f4846
Aniket0195 <aniketgaikwad0195@gmail.com>
2026-06-14 23:52:48 +0530

    Added AdminPage And Admin Test

--- src/main/java/com/orangehrm/pages/AdminPage.java
+++ src/main/java/com/orangehrm/pages/AdminPage.java
@@ -1,15 +1,29 @@
 package com.orangehrm.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.testng.annotations.Test;
+import com.orangehrm.base.BasePage;
 
-public class AdminPage {
+public class AdminPage extends BasePage{
 	
 	public AdminPage(WebDriver driver)
 	{
 		super(driver);
 	}
 	
-	
+	By adminMenu = By.xpath("//span[text()='Admin']");
+	By userName = By.cssSelector("input.oxd-input");
 
+	
+	public void clickAdminMenu() {
+		
+		click(driver.findElement(adminMenu));
+	}
+	
+	public void enterUsername(String user)
+	{
+		type(driver.findElement(userName),user);
+	}
+	
+ 
 }
--- src/test/java/com/orangehrm/tests/AdminTest.java
+++ src/test/java/com/orangehrm/tests/AdminTest.java
@@ -2,30 +2,22 @@ package com.orangehrm.tests;
 
 import org.testng.annotations.Test;
 
-import com.orangehrm.pages.WebElement;
+import com.orangehrm.base.BaseTest;
+import com.orangehrm.pages.AdminPage;
+import com.orangehrm.pages.LoginPage;
 
-public class AdminTest extends LoginTest{
-  @Test
-  public void f() {
-  }
-  
-  @Test
-	public void clickAdminPage() {
-		//WebElement admindashboard = driver.find
-		
-	}
-	
-	
+public class AdminTest extends BaseTest{
+ 
 	@Test
-	public void enterUsername()
-	{
+	public void verifyAdminPage() throws InterruptedException {
+		Thread.sleep(3000);
+		LoginPage loginPage = new LoginPage(driver);
+		loginPage.login("Admin", "admin123");
+		Thread.sleep(3000);
+		AdminPage adminPage = new AdminPage(driver);
 		
-	}
-	
-	@Test
-	public void selectRole()
-	{
-		
+		adminPage.clickAdminMenu();
+		Thread.sleep(3000);
 	}
   
 }
