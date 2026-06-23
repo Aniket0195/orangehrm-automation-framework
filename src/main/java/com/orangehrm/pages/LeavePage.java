525fbef6c3d9125ddaf73c0b7779101894ed7a82
Aniket0195 <aniketgaikwad0195@gmail.com>
2026-06-23 23:09:33 +0530

    Added

--- src/main/java/com/orangehrm/pages/LeavePage.java
+++ src/main/java/com/orangehrm/pages/LeavePage.java
@@ -1,5 +1,14 @@
 package com.orangehrm.pages;
 
-public class LeavePage {
+import org.openqa.selenium.WebDriver;
+
+import com.orangehrm.base.BasePage;
+
+public class LeavePage extends BasePage {
+
+	public LeavePage(WebDriver driver) {
+		super(driver);
+		// TODO Auto-generated constructor stub
+	}
 
 }
